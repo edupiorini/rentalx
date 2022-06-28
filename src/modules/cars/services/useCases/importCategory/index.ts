659f091d832c32eb90bbc020d0cae790d53b88d6
Eduardo Piorini <eduardopiorini@hotmail.com>
2022-06-28 06:19:07 +0200

    refactoring

--- src/modules/cars/services/useCases/importCategory/index.ts
+++ src/modules/cars/services/useCases/importCategory/index.ts
@@ -1,4 +1,3 @@
-import { CategoriesRepository } from '../../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
